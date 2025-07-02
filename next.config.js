68a8b49f4132bf60f056a184ff7ce4a38a52474d
Nestor Bermejillo Gonzalez <n.bermejillo.2021@alumnos.urjc.es>
2025-07-03 00:37:10 +0200

    Fix

--- next.config.js
+++ next.config.js
@@ -1,16 +1,9 @@
+// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: { unoptimized: true },
   output: 'export',
-  // Configura esto con el nombre de tu repositorio
-  basePath: process.env.NODE_ENV === 'production' ? '/NBGonzalez.github.io' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/NBGonzalez.github.io/' : '',
+  basePath: '', // Vacío para repositorio usuario.github.io
+  assetPrefix: '', // Vacío para repositorio usuario.github.io
+  trailingSlash: true, // Recomendado para GitHub Pages
+  // El resto de tu configuración actual...
 };
-
-module.exports = nextConfig;
